224d1d03ca430e5fd1bfd4480fe80aaf4638eeb5
Aleaddin Ozer <ozeraleaddin@gmail.com>
2023-06-09 23:04:31 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use piston::WindowSettings;
+
+fn main(){
+    let settings = WindowSettings::new("Pelekye", (640, 480)).exit_on_esc(true);
+    println!("{}", settings.get_exit_on_esc());
 }
