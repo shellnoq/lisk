986279e40c298f7f284ee8b1bd48f41f2b9ff513
Aleaddin Ozer <ozeraleaddin@gmail.com>
2023-06-10 00:47:45 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use piston::WindowSettings;
+use glutin_window::GlutinWindow;
 
 fn main(){
     let settings = WindowSettings::new("Pelekye", (640, 480)).exit_on_esc(true);
+    let windows: GlutinWindow = settings.build().expect("could not create window");
     println!("{}", settings.get_exit_on_esc());
 }
