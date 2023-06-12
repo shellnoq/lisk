cfd932f187cfcef98ad4cb627a65f782944c809e
Aleaddin Ozer <ozeraleaddin@gmail.com>
2023-06-12 22:50:09 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,5 @@ fn main(){
     let settings = WindowSettings::new("Pelekye", (640, 480)).exit_on_esc(true);
     let windows: GlutinWindow = settings.build().expect("could not create window");
     println!("{}", settings.get_exit_on_esc());
+    
 }
