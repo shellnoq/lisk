0092c2f47124764a226642a0fab66ae05880bfed
Aleaddin Ozer <ozeraleaddin@gmail.com>
2023-06-15 22:41:57 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use glutin_window::GlutinWindow;
 
 fn main(){
     let settings = WindowSettings::new("Pelekye", (640, 480)).exit_on_esc(true);
-    let windows: GlutinWindow = settings.build().expect("could not create window");
+    let _windows: GlutinWindow = settings.build().expect("could not create window");
     println!("{}", settings.get_exit_on_esc());
     
 }
