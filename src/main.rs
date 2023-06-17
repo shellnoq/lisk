95604493b453cd47265bf3b79434609fb5611010
Aleaddin Ozer <ozeraleaddin@gmail.com>
2023-06-17 08:12:32 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use piston::{WindowSettings, Event};
 use glutin_window::GlutinWindow;
+use piston::{WindowSettings, EventLoop};
 use piston::event_loop::{EventSettings, Events};
-use piston::EventLoop;
 
 fn main(){
     let settings = WindowSettings::new("Pelekye", (640, 480)).exit_on_esc(true);
     let mut _windows: GlutinWindow = 
         settings.build().expect("could not create window");
     
-    let mut events = Events::new(EventSettings::new());
+    let mut events = Events::new(EventSettings::new().lazy(true));
 
-        while let Some(e) = events.next(&mut _windows) {
-            
-        }
+    while let Some(e) = events.next(&mut _windows) {
+        
+    }
 
     
 }
